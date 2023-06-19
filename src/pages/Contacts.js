1722060992ab82b4b3fad5505a30058e01a14dc6
Santiago Quintero <santiago.qp86@gmail.com>
2023-06-18 20:02:12 -0500

    style

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Helmet } from "react-helmet";
 import { ContactList } from "components/ContactList/ContactList";
 import { ContactForm } from "components/ContactForm/ContactForm";
 import { Filter} from "components/Filter/Filter";
